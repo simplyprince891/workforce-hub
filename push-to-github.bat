de8e08f48dc76543b14104eb0936ee83a51517c4
simplyprince891 <simplyprince891@gmail.com>
2026-05-13 13:00:43 +0530

    Initial commit: WorkForce Hub - Employee & Task Management System

---
+++ push-to-github.bat
@@ -0,0 +1,9 @@
+@echo off
+cd C:\Users\abdur.ahmed\Desktop\Proj
+git init
+git add .
+git commit -m "Initial commit: WorkForce Hub - Employee & Task Management System"
+git branch -M main
+git remote add origin https://github.com/simplyprince891/workforce-hub.git
+git push -u origin main
+pause
---
+++ setup-git.bat
@@ -0,0 +1,13 @@
+@echo off
+git config --global user.name "simplyprince891"
+git config --global user.email "simplyprince891@gmail.com"
+echo Git configured for simplyprince891!
+echo.
+echo Now run:
+echo git init
+echo git add .
+echo git commit -m "Initial commit"
+echo git remote add origin https://github.com/simplyprince891/workforce-hub.git
+echo git branch -M main
+echo git push -u origin main
+pause
